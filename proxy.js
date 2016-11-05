0f5f841dce79e33423ccac78f5429a989798861e
Luke Chatton <chattonluke@gmail.com>
2016-11-05 10:19:10 -0700

    client

--- proxy.js
+++ proxy.js
@@ -6,6 +6,8 @@ var config = require('./config.json');
 var client = dgram.createSocket("udp4");
 var proxy = new EventEmitter();
 
+var connections = {};
+
 function proxyStart() {
     client.bind(config.proxyPort);
 
@@ -15,8 +17,29 @@ function proxyStart() {
 }
 
 function packetReceive(msg, info) {
-    console.log("tunneling packet!");
-    client.send(msg, 0, msg.length, 30001, "192.99.101.38");
+    console.log("tunneling packet...");
+
+    if (!connections[info.port]) {
+        console.log("new connection!");
+
+        connections[info.port] = {
+            port: info.port,
+            ip: info.address,
+            time: new Date().getTime(),
+            socket: dgram.createSocket("udp4")
+        };
+
+        connections[info.port].socket.bind(info.port);
+        connections[info.port].socket.on("message", function (msg2, info2) {
+            console.log("tunneling returned packet.");
+            client.send(msg2, 0, msg2.length, info2.port, info2.address);
+        })
+    }
+
+    else {
+        client.send(msg, 0, msg.length, 30001, "192.99.101.38");
+    }
+
 }
 
 proxyStart();
