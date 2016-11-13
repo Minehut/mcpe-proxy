404f7c6a2e92c72a836a382050716b75c702b62b
Luke Chatton <chattonluke@gmail.com>
2016-11-12 19:13:07 -0800

    test

--- proxy.js
+++ proxy.js
@@ -56,7 +56,7 @@ function getPacketId(msg) {
 
 
     var hex = msg.toString('hex');
-    var type = hex.substr(0, 1).readUInt8(0);
+    var type = new Buffer(hex.substr(0, 1)).readUInt8(0);
     console.log('type: ' + type);
 
     switch(type) {
