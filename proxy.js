978da216a97f0f37a1d01561cf28198375f83fed
Luke Chatton <chattonluke@gmail.com>
2016-11-12 19:26:34 -0800

    debug

--- proxy.js
+++ proxy.js
@@ -35,36 +35,46 @@ function packetReceive(msg, info) {
 
             connections[info.port].socket.bind(info.port);
             connections[info.port].socket.on("message", function (msg2, info2) {
-                console.log("tunneling player packet to server (" + getPacketId(msg2) + ")");
+                console.log("tunneling player packet to server (" + decodePacket(msg2) + ")");
                 client.send(msg2, 0, msg2.length, info.port, info.address); //send back to player
             });
         }
 
-        console.log("tunneling server packet to player (" + getPacketId(msg) + ")");
+        console.log("tunneling server packet to player (" + decodePacket(msg) + ")");
         connections[info.port].socket.send(msg, 0, msg.length, serverPort, serverIp); //send to server
     } else {
         console.log("Port matched the server ip.");
     }
 }
 
-function getPacketId(msg) {
+function decodePacket(msg) {
     if(typeof(msg) === 'undefined'){
         return "null";
     }
 
-    console.log(inspect(msg));
-
-
     var hex = msg.toString('hex');
     console.log('hex: ' + hex);
     var type = parseInt(hex.substr(0, 1));
-    console.log('type: ' + type);
+
+    var data = {};
+    data.['Packet ID'] = "0x" + type;
 
     switch(type) {
         case 0x01:
             console.log('packet is 0x01!');
             break;
+
+        case 0x1c:
+            data['Ping ID'] = hex.substr(1, 8).toString('hex');
+            data['Server ID'] = hex.substr(9, 8).toString('hex');
+            data['Magic'] = hex.substr(17, 16).toString('hex');
+            data['Length'] = hex.substr(33, 2).readUInt16BE(0);
+            data['Identifier'] = hex.substr(35, 11).toString('ascii');
+            data['Server Name'] = hex.substr(46).toString('ascii');
+            break;
     }
+
+    console.log(inspect(data));
 
 
     return ("0x" + msg.toString('hex')).substring(0, 4);
