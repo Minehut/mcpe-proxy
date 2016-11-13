3cdb53f8dcccd91239685ed024fa8e8488dae8ad
Luke Chatton <chattonluke@gmail.com>
2016-11-12 17:03:36 -0800

    debug

--- proxy.js
+++ proxy.js
@@ -42,12 +42,27 @@ function packetReceive(msg, info) {
         }
 
         console.log("tunneling player packet to server.");
-        console.log(msg);
+        decodePacket(msg);
         console.log('-------------');
         connections[info.port].socket.send(msg, 0, msg.length, serverPort, serverIp); //send to server
     } else {
         console.log("Port patched the server ip!!");
     }
+}
+
+function decodePacket(msg) {
+    var data = {};
+    if(typeof(msg) === 'undefined'){
+        data['Error'] = "No data found for this packet. Maybe the database was cleared?";
+             return data;
+    }
+
+    var type = new Buffer(msg.substr(0,1)).readUInt8(0);
+    var hex = msg;
+    data['Data length'] =  msg.length;
+    data['Packet ID'] = "0x" + msg.substr(0,1).toString('hex');
+
+    console.log('packet type: ' + type);
 }
 
 proxyStart();
